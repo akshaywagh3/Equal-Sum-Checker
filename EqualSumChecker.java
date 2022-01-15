a0540bd30984b09ebaa4710cf5f1760ed0c71f08
Akshay Subhash Wagh <81297179+akshaywagh3@users.noreply.github.com>
2022-01-15 12:32:13 +0530

    Add files via upload

---
+++ EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker
+{
+    public static void main(String[] args)
+    {
+        System.out.println(hasEqualSum(2,9,10));
+    }
+    public static boolean hasEqualSum(int a,int b, int c)
+    {
+        return (a+b)==c;
+    }
+}
